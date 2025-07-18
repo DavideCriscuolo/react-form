02aaed4db57039925c5a1d4478f016352a6bd6f1
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-18 11:21:58 +0200

    creato il file contente la lista di articoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+Milestone 1
+Creare una pagina che visualizzi una lista di articoli, mostrandone solo il titolo.
+
+Milestone 2
+Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un nuovo articolo del blog. Al submit del form, mostrare la lista degli articoli aggiornati.
+
+BONUS
+Aggiungere la possibilità di cancellare ciascun articolo utilizzando un'icona.
+Implementare la funzionalità di modifica del titolo di un post.
+
+*/
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
